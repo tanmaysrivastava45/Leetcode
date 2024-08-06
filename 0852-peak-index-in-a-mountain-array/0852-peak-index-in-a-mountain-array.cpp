6f358a0748f37fbf526a91942795edf36039a1bd
tanmaysrivastava45 <168894520+tanmaysrivastava45@users.noreply.github.com>
2024-08-06 17:43:25 +0530

    Time: 84 ms (39.79%), Space: 62.3 MB (34.06%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
@@ -5,17 +5,28 @@ public:
         int n=arr.size();
         int end=n-1;
         int mid= start+ (end-start)/2;
-        while(start<=end)
+        // while(start<=end)
+        // {
+        //     if(arr[mid]<arr[mid+1])
+        //     {
+        //         start=mid+1;
+        //     }
+        //     else if(arr[mid-1]>arr[mid])
+        //     {
+        //         end=mid-1;
+        //     }
+        //     else{ return mid; }
+        //     mid=start+(end-start)/2;
+        // }
+        // return mid;
+        while(start<end)
         {
             if(arr[mid]<arr[mid+1])
             {
                 start=mid+1;
             }
-            else if(arr[mid-1]>arr[mid])
-            {
-                end=mid-1;
-            }
-            else{ return mid; }
+            else{
+               end=mid;}
             mid=start+(end-start)/2;
         }
         return mid;
