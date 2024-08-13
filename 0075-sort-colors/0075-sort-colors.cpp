73d3d0d67665baa980ac95ecffcbb08fd7ef0ead
tanmaysrivastava45 <168894520+tanmaysrivastava45@users.noreply.github.com>
2024-08-13 15:31:12 +0530

    Time: 0 ms (100.00%), Space: 10.2 MB (18.45%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,18 +1,40 @@
+// selection sorting 
+// class Solution {
+// public:
+//     void sortColors(vector<int>& nums) {
+//         int n=nums.size();
+//         for(int i=0;i<n-1;i++)
+//         {
+//             int minindex=i;
+//             for(int j=i+1;j<n;j++)
+//             {
+//                 if(nums[j]<nums[minindex])
+//                 {
+//                     minindex=j;
+//                 }
+//             }
+//             swap(nums[i],nums[minindex]);
+//         }
+//     }
+// };
+
+
+// bubble sorting
+
+
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        int n=nums.size();
-        for(int i=0;i<n-1;i++)
+        int n= nums.size();
+        for(int i=1;i<n;i++)
         {
-            int minindex=i;
-            for(int j=i+1;j<n;j++)
+            for(int j=0;j<n-i;j++)
             {
-                if(nums[j]<nums[minindex])
+                if(nums[j]>nums[j+1])
                 {
-                    minindex=j;
+                    swap(nums[j],nums[j+1]);
                 }
             }
-            swap(nums[i],nums[minindex]);
         }
     }
 };
