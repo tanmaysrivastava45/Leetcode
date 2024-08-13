f59410fca9132e7bc0c614160ab06e1e106014ef
tanmaysrivastava45 <168894520+tanmaysrivastava45@users.noreply.github.com>
2024-08-13 12:56:08 +0530

    Time: 0 ms (100.00%), Space: 10.2 MB (18.45%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int n=nums.size();
+        for(int i=0;i<n-1;i++)
+        {
+            int minindex=i;
+            for(int j=i+1;j<n;j++)
+            {
+                if(nums[j]<nums[minindex])
+                {
+                    minindex=j;
+                }
+            }
+            swap(nums[i],nums[minindex]);
+        }
+    }
+};
