c7b93f0e1e0b3536610c8d546fd88af1004f17be
tanmaysrivastava45 <168894520+tanmaysrivastava45@users.noreply.github.com>
2024-08-14 19:22:43 +0530

    0026-remove-duplicates-from-sorted-array.cpp

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -2,14 +2,14 @@ class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
         int n=nums.size();
-        int newarray=1;
+        int newarray=1;  //set the newarray length as 1;
        
-        for(int i=1;i<n;i++)
+        for(int i=1;i<n;i++) // we are assuming that first element is already sorted 
         {
-            if(nums[i]!=nums[i-1])
+            if(nums[i]!=nums[i-1]) /// we are taking i-1 not i+1 because it produces buffer flow error 
             {
                 nums[newarray]=nums[i];
-                newarray++;
+                newarray++;      // increasing the length of array as move ahead in iteration 
             }
         }
             
