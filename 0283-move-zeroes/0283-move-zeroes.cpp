023d4d72b198f59f1b756cc7d4e74b8f5499e4ea
tanmaysrivastava45 <168894520+tanmaysrivastava45@users.noreply.github.com>
2024-08-16 00:05:19 +0530

    Time: 20 ms (26.53%), Space: 22.4 MB (7.87%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,30 +1,38 @@
-class Solution {
-public:
-    void moveZeroes(vector<int>& nums) {
-        int n=nums.size();
-       int j=0;
-        for(int i=0;i<n;i++)
-        {
-            
-            if(nums[i]!=0)
-            {
-                swap(nums[j],nums[i]);
-                j++;
-                
-            }
-        }
-    }
-};
 // class Solution {
 // public:
 //     void moveZeroes(vector<int>& nums) {
-//         int left = 0;
-
-//         for (int right = 0; right < nums.size(); right++) {
-//             if (nums[right] != 0) {
-//                 swap(nums[right], nums[left]);
-//                 left++;
+//         int n=nums.size();
+//        int j=0;
+//         for(int i=0;i<n;i++)
+//         {
+            
+//             if(nums[i]!=0)
+//             {
+//                 swap(nums[j],nums[i]);
+//                 j++;
+                
 //             }
-//         }        
+//         }
 //     }
 // };
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        vector<int>arr(nums.size());
+        int index=0;
+        for(int i : nums)
+        {
+            if(i!=0)
+            {
+                arr[index]=i;
+                index++;
+            }
+        }
+        while(index<nums.size())
+        {
+            arr[index]=0;
+            index++;
+        }
+        nums=arr;
+    }
+};
