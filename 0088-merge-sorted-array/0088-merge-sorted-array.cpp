1d830fdd4debbdccb0db04a0c8445c58c7c27809
tanmaysrivastava45 <168894520+tanmaysrivastava45@users.noreply.github.com>
2024-08-15 19:52:04 +0530

    Time: 0 ms (100.00%), Space: 11.4 MB (14.23%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -38,3 +38,26 @@ public:
         }
     }
 };
+// another approah
+// class Solution {
+// public:
+//     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+//         // Start merging from the end of nums1 to avoid overwriting
+//         int i = m - 1; // Last index of the initialized part of nums1
+//         int j = n - 1; // Last index of nums2
+//         int k = m + n - 1; // Last index of nums1
+        
+//         while (i >= 0 && j >= 0) {
+//             if (nums1[i] > nums2[j]) {
+//                 nums1[k--] = nums1[i--];
+//             } else {
+//                 nums1[k--] = nums2[j--];
+//             }
+//         }
+        
+//         // If there are remaining elements in nums2, copy them
+//         while (j >= 0) {
+//             nums1[k--] = nums2[j--];
+//         }
+//     }
+// };
