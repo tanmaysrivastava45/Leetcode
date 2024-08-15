186cb9ea82fabb1d507afd5d2e47fa9f098ff047
tanmaysrivastava45 <168894520+tanmaysrivastava45@users.noreply.github.com>
2024-08-15 17:05:09 +0530

    1232-check-if-it-is-a-straight-line.cpp

--- 1232-check-if-it-is-a-straight-line/1232-check-if-it-is-a-straight-line.cpp
+++ 1232-check-if-it-is-a-straight-line/1232-check-if-it-is-a-straight-line.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
     bool checkStraightLine(vector<vector<int>>& coordinates) {
-        
+        // we will be solving this question with the help of slope of a line 
         int n=coordinates.size();
         int x1=coordinates[0][0],y1=coordinates[0][1];
         int x2=coordinates[1][0],y2=coordinates[1][1];
-        int dx=x2-x1;
+        int dx=x2-x1;//calculating slope of the line 
         int dy=y2-y1;
         for(int i=2;i<n;i++)
         {
+            // a loop to check whether it is valid for all points 
             int x=coordinates[i][0];
             int y=coordinates[i][1];
             
