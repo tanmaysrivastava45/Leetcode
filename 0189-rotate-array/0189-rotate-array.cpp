54cba4a2618df2919bf8e16cd7fd3b78d873c871
tanmaysrivastava45 <168894520+tanmaysrivastava45@users.noreply.github.com>
2024-08-16 12:59:43 +0530

    0189-rotate-array.cpp

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -5,7 +5,7 @@ public:
         vector<int> temp(n);
         for(int i=0;i<n;i++)
         {
-            temp[(i+k)%n]=nums[i];
+            temp[(i+k)%n]=nums[i];  // this is formula for rotating array 
         }
         nums=temp;
     }
