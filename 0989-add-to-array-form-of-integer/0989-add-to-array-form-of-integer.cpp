4ba40902623f38c7277a4c543836225cf526c7d7
tanmaysrivastava45 <168894520+tanmaysrivastava45@users.noreply.github.com>
2024-08-17 08:50:23 +0530

    Time: 15 ms (80.34%), Space: 31.5 MB (15.68%) - LeetHub

---
+++ 0989-add-to-array-form-of-integer/0989-add-to-array-form-of-integer.cpp
@@ -0,0 +1,48 @@
+// this method was good but it provides an overflow condition when the array has a lot of elements
+// class Solution {
+// public:
+//     vector<int> addToArrayForm(vector<int>& num, int k) {
+//         vector<int>v;
+//         long long int ans=0;
+//         for(int i=0;i<num.size();i++)
+//         {
+//             ans=ans*10+num[i];
+//         }
+//         long long int sum=ans+k;
+//         int digit=0;
+//         // if(sum==0)
+//         // {
+//         //     return {0};
+//         // }
+//         while(sum>0)
+//         {
+//             digit=sum%10;
+//             sum=sum/10;
+//             v.push_back(digit);
+//         }
+//         reverse(v.begin(),v.end());
+//         return v;
+//     }
+// };
+
+class Solution {
+public:
+    vector<int> addToArrayForm(vector<int>& num, int k) {
+        vector<int>v;
+        int n=num.size();
+        int i=n-1;
+        int carry=k;
+        while(i>=0 || carry>0 )
+        {
+            if(i>=0)
+            {
+                carry=carry+num[i];
+                i--;
+            }
+            v.push_back(carry%10);
+            carry=carry/10;
+        }
+        reverse(v.begin(),v.end());
+        return v;
+    }
+};
