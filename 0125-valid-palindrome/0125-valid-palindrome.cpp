41a1c6109fb272e0d4dd83bb799ba328b0486ff0
tanmaysrivastava45 <168894520+tanmaysrivastava45@users.noreply.github.com>
2024-08-19 13:13:20 +0530

    Time: 6 ms (44.91%), Space: 9.7 MB (9.02%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,23 +1,103 @@
+// easy solution using built in function 
+
+// class Solution {
+// public:
+//     bool isPalindrome(string s) {
+//         int b=0;
+//         int e=s.size()-1;
+//         while(b<e)
+//         {
+//             while(b<e && !isalnum(s[b]))
+//             {
+//                 b++;
+//             }
+//             while(b<e && !isalnum(s[e]))
+//             {
+//                 e--;
+//             }
+//             if(tolower(s[b])!=tolower(s[e]))
+//                 return false;
+//             b++;
+//             e--;
+//         }
+//         return true;
+//     }
+// };
+
 class Solution {
 public:
-    bool isPalindrome(string s) {
-        int b=0;
-        int e=s.size()-1;
-        while(b<e)
+    
+    bool valid(char ch)
+    {
+        if((ch>='a'&& ch<='z')||(ch>='A'&& ch<='Z')||(ch>='0' && ch<='9'))
+        {return 1;}
+        return 0;
+    }
+    
+    
+    char tolowercase(char ch)
+    {
+        if((ch>='a'&& ch<='z')||(ch>='0' && ch<='9'))
         {
-            while(b<e && !isalnum(s[b]))
-            {
-                b++;
-            }
-            while(b<e && !isalnum(s[e]))
-            {
-                e--;
-            }
-            if(tolower(s[b])!=tolower(s[e]))
-                return false;
-            b++;
-            e--;
+            return ch;
         }
-        return true;
+        else
+        {
+            char temp= ch-'A'+'a';
+            return temp;
+        }
+    }
+    
+    bool checkpalindrome(string a)
+    {
+        int s=0;
+        int e=a.size()-1;
+        while(s<e)
+        {
+        if(a[s]!=a[e])
+        {
+            return 0;
+        }
+        s++;
+        e--;
+        }
+        return 1;
+    }
+    
+    bool isPalindrome(string s) {
+        string temp="";
+        for(int i=0;i<s.size();i++)
+        {
+            if(valid(s[i]))
+            {
+                temp.push_back(s[i]);
+            }
+        }
+        for(int i=0;i<temp.size();i++)
+        {
+            temp[i]=tolowercase(temp[i]);
+        }
+        return checkpalindrome(temp);
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
