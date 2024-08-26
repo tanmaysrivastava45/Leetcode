211a0ba18675d633cdb0d21a656389563a46f65d
tanmaysrivastava45 <168894520+tanmaysrivastava45@users.noreply.github.com>
2024-08-26 22:21:07 +0530

    Time: 43 ms (63.72%), Space: 51.2 MB (37.46%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -3,6 +3,7 @@ public:
     bool canJump(vector<int>& nums) {
        //we will check if arr[i] + i is >= nums.size()
         int reach = 0;
+        // it is some great knoeledge 
         
         for(int i=0;i<=reach;i++){
             
