0726b8df781f649e685e454c44280cf2973dd6e0
tanmaysrivastava45 <168894520+tanmaysrivastava45@users.noreply.github.com>
2024-09-12 12:27:22 +0530

    Time: 3 ms (41.43%), Space: 7.4 MB (89.60%) - LeetHub

--- 0202-happy-number/0202-happy-number.cpp
+++ 0202-happy-number/0202-happy-number.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     bool isHappy(int n) {
         
-        while(n!=1 && n!=4)
+        while(n!=1 && n!=4) // 4 has to be included as it has been seen repeating however there is an other                method which we will do later that is with unorderd map 
         {
        long long int sum=0;
         while(n>0)
