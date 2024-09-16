10cb7ec92bd2ac513df4dbe01ad309b5024fbf05
tanmaysrivastava45 <168894520+tanmaysrivastava45@users.noreply.github.com>
2024-09-16 19:30:00 +0530

    Time: 0 ms (100.00%), Space: 8.2 MB (70.65%) - LeetHub

--- 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.cpp
+++ 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.cpp
@@ -1,8 +1,14 @@
 class Solution {
 public:
     string removeOccurrences(string s, string part) {
+        
+        // in this problem we use some inbuilt functions of string like erase and find 
+        
       while(s.length()!=0 && s.find(part)<s.length())
       {
+          
+          // we will erase that part which is in the string and is a part of the string 
+          
           s.erase(s.find(part),part.length());
       }
         return s;
