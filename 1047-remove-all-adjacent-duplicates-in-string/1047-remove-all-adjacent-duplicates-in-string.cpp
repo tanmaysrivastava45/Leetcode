91100bcf0a0bfa84d03996701d0355cfb4fb2cde
tanmaysrivastava45 <168894520+tanmaysrivastava45@users.noreply.github.com>
2024-09-21 13:31:24 +0530

    Time: 57 ms (19.22%), Space: 11.3 MB (96.22%) - LeetHub

--- 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
@@ -2,12 +2,12 @@ class Solution {
 public:
     string removeDuplicates(string s) {
          int i=1;
-       while(i<s.length())
+       while(i<s.length()) 
        {
            if(s[i-1]==s[i])
            {
                s.erase(i-1,2);
-               if(i>1)
+               if(i>1)  /// to recheck whether there is any duplicate character
                {
                    i--;
                }
