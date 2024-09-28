d61bcf230f1e35c6b25b0a1e337a56163b0f73e4
tanmaysrivastava45 <168894520+tanmaysrivastava45@users.noreply.github.com>
2024-09-28 16:37:26 +0530

    Time: 496 ms (77.84%), Space: 288.6 MB (74.76%) - LeetHub

---
+++ 3295-report-spam-message/3295-report-spam-message.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool reportSpam(vector<string>& message, vector<string>& bannedWords) {
+        
+        unordered_set<string> bannedSet(bannedWords.begin(), bannedWords.end());
+
+        int count = 0;
+
+       
+        for (const auto& word : message)
+        {
+            if (bannedSet.find(word) != bannedSet.end()) 
+            {
+                count++;
+               
+                if (count >= 2)
+                {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+};
