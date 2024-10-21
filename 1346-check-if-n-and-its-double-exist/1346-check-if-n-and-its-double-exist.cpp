027e7c545796e047e47779859899679d072ef4f7
tanmaysrivastava45 <168894520+tanmaysrivastava45@users.noreply.github.com>
2024-10-22 00:33:16 +0530

    Time: 0 ms (100.00%), Space: 12.8 MB (54.58%) - LeetHub

--- 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.cpp
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.cpp
@@ -1,18 +1,38 @@
+// class Solution {
+// public:
+//     bool checkIfExist(vector<int>& arr) {
+// //         int n=arr.size();
+// //         // for(int i=0;i<n;i++)
+// //         // {
+// //         //     for(int j=i+1;j<n;j++)
+// //         //     {
+// //         //         if(arr[i]==2*arr[j] && i!=j || arr[j]==2*arr[i])
+// //         //         {
+// //         //             return 1;
+// //         //             break;
+// //         //         }
+// //         //     }
+// //         // }
+      
+// //         return 0;
+        
+//     }
+// };
+
+
 class Solution {
 public:
     bool checkIfExist(vector<int>& arr) {
-        int n=arr.size();
-        for(int i=0;i<n;i++)
-        {
-            for(int j=i+1;j<n;j++)
-            {
-                if(arr[i]==2*arr[j] && i!=j || arr[j]==2*arr[i])
-                {
-                    return 1;
-                    break;
+        sort(arr.begin(), arr.end());
+        int n = arr.size();  
+        for (int i = 0; i < n; i++) {
+            if (binary_search(arr.begin(), arr.end(), 2 * arr[i])) {
+                if (find(arr.begin(), arr.end(), 2 * arr[i]) != arr.begin() + i) {
+                    return true;
                 }
             }
         }
-        return 0;
+
+        return false;
     }
 };
