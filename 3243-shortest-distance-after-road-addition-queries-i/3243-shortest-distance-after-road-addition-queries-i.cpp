73eb619482627f833e9422c95c19a2959081860b
Tanmay Srivastava <168894520+tanmaysrivastava45@users.noreply.github.com>
2024-11-27 14:49:42 +0530

    Time: 31 ms (89.76%), Space: 51.7 MB (99.02%) - LeetHub

---
+++ 3243-shortest-distance-after-road-addition-queries-i/3243-shortest-distance-after-road-addition-queries-i.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> shortestDistanceAfterQueries(int n, vector<vector<int>>& queries) {
+    vector<vector<int>> roads(n);
+    vector<int> dp(n), res;
+    iota(begin(dp), end(dp), 0);
+    for (const auto &q : queries) {
+        roads[q[1]].push_back(q[0]);
+        for (int i = q[1]; i < n; ++i) {
+            dp[i] = min(dp[i], dp[i - 1] + 1);
+            for (int l : roads[i])
+                dp[i] = min(dp[i], dp[l] + 1);
+        }
+        res.push_back(dp[n - 1]);
+    }
+    return res;
+    }
+};
