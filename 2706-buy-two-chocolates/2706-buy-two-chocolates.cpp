0ca3061dc5293dc1ca00da4873ce512c55926db3
Tanmay Srivastava <168894520+tanmaysrivastava45@users.noreply.github.com>
2024-12-21 21:27:35 +0530

    Time: 0 ms (100.00%), Space: 52.2 MB (13.06%) - LeetHub

--- 2706-buy-two-chocolates/2706-buy-two-chocolates.cpp
+++ 2706-buy-two-chocolates/2706-buy-two-chocolates.cpp
@@ -2,24 +2,17 @@ class Solution {
 public:
     int buyChoco(vector<int>& prices, int money) {
         int n=prices.size();
-        int sum=0;
         int count=0;
         sort(prices.begin(),prices.end());
-        for(int i=0;i<n;i++)
-        {
-            if(sum<money)
-            {
-            sum=sum+prices[i];
-                if(sum<=money){
-                     count++;
-                }
-            }
-            if(count==2)break;
-        }
-        if(count!=2)
-        {
-            return money;
-        }
-        return money-sum;
+        int sum=prices[0]+prices[1];
+           if(sum<=money)
+           {
+               return money-sum;
+           }
+         else 
+         {
+             return money;
+         }
+     
     }
 };
