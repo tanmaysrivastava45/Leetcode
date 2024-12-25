1e3dadfb87f48a42c7a02238c8b59be069f4df2f
Tanmay Srivastava <168894520+tanmaysrivastava45@users.noreply.github.com>
2024-12-25 12:58:35 +0530

    Time: 8 ms (33.71%), Space: 71.4 MB (64.23%) - LeetHub

---
+++ 0848-shifting-letters/0848-shifting-letters.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    string shiftingLetters(string s, vector<int>& shifts) {
+        int n=shifts.size();
+        long long int sum=0;
+        for(int i=0;i<n;i++)
+        {
+            sum=(sum+shifts[i])%26;
+        }
+        long long int lastsum=0;
+        for(int i=0;i<n;i++)
+        {
+            if(i>0)
+        {
+            lastsum = (lastsum%26 + shifts[i-1]%26)%26;
+        }
+            int res=(sum%26+(s[i]-'a')%26-lastsum%26)%26;
+            if(res<0)
+            {
+                res = res + 26;
+            }
+            s[i]=(res) +'a';
+        }
+        return s;
+    }
+};
